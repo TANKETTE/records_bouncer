8f6497ea4ec58d58a0a5625b52e1756bd80e4c63
TANKETTE <TANKETTE@users.noreply.github.com>
2017-11-20 15:54:49 +0800

    2017-11-20

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -4,22 +4,38 @@
 <hr>
     {!! Form::open(['method' => 'POST', 'route' => ['customers.store']]) !!}
 
+<div class="row">
+        <div class="col-md-1 form-group">
+            {!! Form::submit('Save', ['class' => 'btn btn-success btn-lg']) !!}
+        </div>
+        <div class="col-md-1 form-group">
+            {!! Form::submit('Cancel', ['class' => 'btn btn-danger btn-lg']) !!}
+        </div>
+    </div>
+
 <div class="panel panel-default">
     <div class="panel-body">
             <div class="row">
-                <div class="col-xs-12 form-group">
+                <div class="col-lg-6 form-group">
                     {!! Form::label('name', 'Name (required)', ['class' => 'control-label']) !!}
                     {!! Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'Name', 'required' => '']) !!}
-                    <br>
+                </div>
+                    
+                <div class="col-lg-6 form-group">
                     {!! Form::label('ic', 'Ic Number', ['class' => 'control-label']) !!}
                     {!! Form::text('ic', '', ['class' => 'form-control', 'placeholder' => 'Ic Number']) !!}
-                    <br>
+                </div>
+
+                <div class="col-lg-6 form-group">    
                     {!! Form::label('phone1', 'Phone Number 1 (required)', ['class' => 'control-label']) !!}
                     {!! Form::text('phone1', '', ['class' => 'form-control', 'placeholder' => 'Phone Number 1', 'required' => '']) !!}
-                    <br>
+                </div>
+                <div class="col-lg-6 form-group">    
                     {!! Form::label('phone2', 'Phone Number 2', ['class' => 'control-label']) !!}
                     {!! Form::text('phone2', '', ['class' => 'form-control', 'placeholder' => 'Phone Number 2']) !!}
-                    <br>
+                </div>
+
+                <div class="col-lg-6 form-group">    
                     {!! Form::label('phone3', 'Phone Number 3', ['class' => 'control-label']) !!}
                     {!! Form::text('phone3', '', ['class' => 'form-control', 'placeholder' => 'Phone Number 3']) !!}
                 </div>
@@ -27,8 +43,7 @@
             
         </div>
     </div>
-
-    {!! Form::submit('Save', ['class' => 'btn btn-danger']) !!}
+    
     {!! Form::close() !!}
 
     
--- resources/views/sclrecords/create.blade.php
+++ resources/views/sclrecords/create.blade.php
@@ -1,21 +1,63 @@
 @extends('layouts.app')
 @section('content')
-<h3 class="page-title">Create New Scl Record</h3>
+
+
 <hr>
     {!! Form::open(['method' => 'POST', 'route' => ['sclrecords.store']]) !!}
-    {!! Form::submit('Save', ['class' => 'btn btn-danger']) !!}
+    {!!Form::hidden('customer_id', $customer_id)!!}
+<div class="row">
+    <div class="col-md-8 form-group">
+        <h2>Create New Scl Record</h2>
+    </div>
+    <div class="col-md-1 form-group">
+        {!! Form::submit('Save', ['class' => 'btn btn-success btn-lg']) !!}
+    </div>
+    <div class="col-md-1 form-group">
+        {!! Form::submit('Cancel', ['class' => 'btn btn-danger btn-lg']) !!}
+    </div>
+</div>
     <hr>
     <div class="panel panel-default">
-
         <div class="panel-body">
             <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('history', 'History', ['class' => 'control-label']) !!}
-                    {!! Form::textarea('history', '', ['class' => 'form-control', 'placeholder' => 'History']) !!}    
+                <div class="col-md-3 form-group">
+                    {!! Form::label('name', 'Name', ['class' => 'control-label']) !!}
+                    {!! Form::text('name', old('title'), ['class' => 'form-control', 'placeholder' => '','readonly']) !!} 
+                </div>
+                <div class="col-md-3 form-group">
+                    {!! Form::label('ic', 'IC Number', ['class' => 'control-label']) !!}
+                    {!! Form::text('ic', old('title'), ['class' => 'form-control', 'placeholder' => '', 'readonly']) !!}   
+                </div>
+                <div class="col-md-3 form-group">
                     {!! Form::label('visit_date', 'Visit Date', ['class' => 'control-label']) !!}
-                    {!! Form::date('visit_date', '', ['class' => 'form-control']) !!}    
+                    {!! Form::date('visit_date', '', ['class' => 'form-control']) !!} 
+                </div>
+            </div>
 
-                    {!!Form::hidden('customer_id', $customer_id)!!}
+            <div class="row">
+                <div class="col-md-3 form-group">
+                    {!! Form::label('phone1', 'Phone Number 1', ['class' => 'control-label']) !!}
+                    {!! Form::text('phone1', old('title'), ['class' => 'form-control', 'placeholder' => '','readonly']) !!}   
+                </div>
+                <div class="col-md-3 form-group">
+                    {!! Form::label('phone2', 'Phone Number 2', ['class' => 'control-label']) !!}
+                    {!! Form::text('phone2', old('title'), ['class' => 'form-control', 'placeholder' => '','readonly']) !!}   
+                </div>
+                <div class="col-md-3 form-group">
+                    <div class="col-xs-12 form-group">
+                    {!! Form::label('phone3', 'Phone Number 3', ['class' => 'control-label']) !!}
+                    {!! Form::text('phone3', old('title'), ['class' => 'form-control', 'placeholder' => '','readonly']) !!}   
+                </div>
+            </div>
+                
+            </div>
+            <div class="row">
+                <div class="col-md-10 form-group">
+                    {!! Form::label('history', 'History', ['class' => 'control-label']) !!}
+                    {!! Form::textarea('history', '', ['class' => 'form-control', 'placeholder' => 'History']) !!}
+                </div> 
+            </div>                          
+                    
         </div>
     </div>
 
